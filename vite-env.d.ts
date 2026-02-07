6b7baab29de7324cde9155f897c4cebd5ae0c871
brunomaytec-commits <brunomaytec@gmail.com>
2026-02-07 17:33:54 -0300

    Delete vite-env.d.ts

--- vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-// Fix for "Cannot find type definition file for 'vite/client'"
-// Manually defining process.env used in the application
-declare const process: {
-  env: {
-    [key: string]: string | undefined;
-    API_KEY: string;
-  }
-};
